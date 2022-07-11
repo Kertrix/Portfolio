7c3d1b55bf87f870966b69cc44fe6f585e331848
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2022-07-11 20:28:36 +0200

    Observer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,19 @@
-skills = document.getElementById('skills')
-elements = skills.querySelectorAll(".skill")
 
-var observer = new IntersectionObserver(function(entries) {
+let observer = new IntersectionObserver(function(entries) {
 	if(entries[0].isIntersecting === true)
         element = entries[0]["target"]
         if(element) {
-            console.log(element)
             element.classList.add(element.id)
         }
 }, { threshold: [1] });
 
+skills = document.getElementById('skills')
+elements = skills.querySelectorAll(".skill")
 
-elements.forEach((e) => {
-    observer.observe(e);
-})
+
+observer.observe(elements[0]);
+observer.observe(elements[1]);
+observer.observe(elements[2]);
+observer.observe(elements[3]);
+observer.observe(elements[4]);
 
